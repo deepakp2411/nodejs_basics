0285ac1a289b1092fb0b727121a8a98152d3b0fa
Deepak <bbaz2deepak@gmail.com>
2022-07-06 11:24:56 +0530

    added readme

---
+++ README.md
@@ -0,0 +1,17 @@
+## Starts Node JS 
+
+```Javascript
+global no window
+
+__dirname -- path to current directory 
+__filename     -file name
+require -- function to use modules ( common js)
+module  -- info about current module (file)
+process -- info about env where th progress is being executed.
+
+
+
+
+```
+ 
+
--- app.js
+++ app.js
@@ -7,10 +7,21 @@
 // }
 // console.log("Hy this is my first app")
 
-const namesd = require('./names')
-const sayHi = require('./utils')
-console.log(namesd)
+// const namesd = require('./names')
+// const sayHi = require('./utils')
+// console.log(namesd)
 
-sayHi("Batman")
-sayHi(namesd.deepak)
-sayHi(namesd.sam)
+// sayHi("Batman")
+// sayHi(namesd.deepak)
+// sayHi(namesd.sam)
+
+// const {readFileSync, writeFileSync} = require('fs');
+
+// const first = readFileSync('./content/first.txt','utf-8')
+// const second = readFileSync('./content/second.txt','utf-8')
+
+// console.log(first,second)
+
+// writeFileSync(
+//     './content/result.txt',`The result is: ${first},${second}`
+// )
